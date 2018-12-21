e1ba978cb7b87036ba8bc92250fb1fccd48003fa
Rachel Pierce <rachelpierce@students.berkeley.net>
2018-12-21 13:21:43 -0800

    a

--- zoo.js
+++ zoo.js
@@ -1,9 +1,19 @@
 var favoriteFood="bacon";
 
 function sleep(name){
-    console.log(name + " sleeps for 8 hours")
+    console.log(name + " sleeps for 8 hours");
 }
 
 function run(){
     sleep("Tigger");
+}
+
+function eat(name,food) {
+    console.log(name+" eats "+food);
+    if(food==favoriteFood){
+        console.log(name+" wants more "+food);
+    }if(food!=favoriteFood){
+        console.log
+    }
+
 }
