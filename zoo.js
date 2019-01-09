c76edbaf46acd1c6c05f9595bbced1a299221fd2
Rachel Pierce <rachelpierce@students.berkeley.net>
2019-01-09 13:06:41 -0800

    2nd try

--- zoo.js
+++ zoo.js
@@ -1,19 +1,104 @@
-var favoriteFood="bacon";
 
-function sleep(name){
-    console.log(name + " sleeps for 8 hours");
-}
+var animalPopulation=0;
 
 function run(){
-    sleep("Tigger");
+    var tigger=new Tiger("Tigger");
+    tigger.eat("meat");
+    var pooh=new Bear("Pooh");
+    pooh.eat("fish");
+    pooh.eat("meat");
+    var rarity=new Unicorn("Rarity")
+    rarity.eat("marshmallows");
+    var gemma=new Giraffes("Gemma");
+    gemma.eat("meat");
+    gemma.eat("leaves");
+    var stinger=new Bee("Stinger");
+    stinger.eat("ice cream");
+    stinger.eat("pollen");
+    var zoebot=new Zookeeper("zoebot");
+    zoebot.feedAnimals([Tiger,Unicorn,Bear,Bee],"meat");
 }
 
-function eat(name,food) {
-    console.log(name+" eats "+food);
-    if(food==favoriteFood){
-        console.log(name+" wants more "+food);
-    }if(food!=favoriteFood){
-        console.log
+class Zookeeper{
+    constructor(name){
+        this.name=name;
     }
 
+    feedAnimals(animals,food){
+        console.log(this.name+" is feeding "+food+" to "+animals.length+" animals"+" of "+animalPopulation+" total animals");
+    }
+
+}
+
+class Animal{
+    constructor(name,favoriteFood){
+        this.name=name;
+        this.favoriteFood=favoriteFood;
+        animalPopulation++;
+    }
+    sleep(sleepTime){
+        console.log(this.name + " sleeps for 8 hours");
+    }
+    eat(food){
+        console.log(this.name+" eats "+food);
+        food==this.favoriteFood?console.log("YUM!! "+this.name+" wants more "+food):this.sleep();
+    }
+    static getPopulation() {
+        return animalPopulation;
+    }
+
+}
+
+class Tiger extends Animal{
+    constructor(name){
+        super(name,"meat");
+    }
+    sleep(){
+        super.sleep(" sleeps for 8 hours");
+    }
+}
+
+class Bear extends Animal{
+    constructor(name){
+        super(name,"fish");
+    }
+    sleep(){
+       console.log(this.name +"  hibernates for 4 months");
+    }
+}
+
+class Unicorn extends Animal{
+    constructor(name){
+        super(name,"marshmallows");
+    }
+    sleep(){
+        console.log(this.name +" sleeps in a cloud");
+    }
+    eat(food){
+        console.log(this.name+" eats "+food);
+        food==this.favoriteFood?console.log("YUM!! "+this.name+" wants more "+food)+this.sleep():("");
+    }
+}
+
+class Giraffes extends Animal{
+    constructor(name){
+        super(name,"leaves");
+    }
+    eat(food){
+        console.log(this.name+" eats "+food);
+        food==this.favoriteFood?console.log("YUM!! "+this.name+" wants more "+food)+this.sleep():console.log("YUCK!!! "+name+" will not eat "+food);
+    }
+}
+
+class Bee extends Animal{
+    constructor(name){
+        super(name,"pollen");
+    }
+    sleep(){
+        console.log(this.name +"  never sleeps");
+    }
+    eat(food){
+        console.log(this.name+" eats "+food);
+        food==this.favoriteFood?console.log("YUM!! "+this.name+" wants more "+food)+this.sleep():console.log("YUCK!!! "+name+" will not eat "+food);
+    }
 }
